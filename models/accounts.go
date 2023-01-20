095edf6dc908749acac79a862ba2c643f84c99fb
Samuel Swandi <samuel.swandi@tokopedia.com>
2023-01-20 19:08:16 +0700

    feat: change pk to string

--- models/accounts.go
+++ models/accounts.go
@@ -3,7 +3,7 @@ package models
 import "itfest-backend-2.0/types"
 
 type Accounts struct {
-	Id       int64                 `gorm:"primaryKey"`
+	Id       string                `gorm:"primaryKey"`
 	Username string                `gorm:"not null;unique"`
 	Password types.EncryptedString `gorm:"not null"`
 	Role     types.Role            `gorm:"not null"`
