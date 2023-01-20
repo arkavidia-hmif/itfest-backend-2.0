c1c865c9471828092418f832ab89e42d81648250
Samuel Swandi <samuel.swandi@tokopedia.com>
2023-01-20 18:12:42 +0700

    feat: add local development

---
+++ configs/database.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	host := os.Getenv("DEV_HOST")
+	port := os.Getenv("DEV_PORT")
+	dbname := os.Getenv("DEV_DBNAME")
+	username := os.Getenv("DEV_USERNAME")
+	password := os.Getenv("DEV_PASSWORD")
+
+	dsn := "host=" + host
+	dsn += " user=" + username
+	dsn += " password=" + password
+	dsn += " dbname=" + dbname
+	dsn += " port=" + port
+	dsn += " sslmode=disable"
+
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		panic("Cannot connect database")
+	}
+	DB.AutoMigrate()
+
+	fmt.Println("Database connected")
+}
