b662aafcaaedc7b4ec18f7b2218a4ffcaf4512a7
Samuel Swandi <samuel.swandi@tokopedia.com>
2023-02-08 18:31:59 +0700

    feat: change config

--- configs/database.go
+++ configs/database.go
@@ -17,19 +17,19 @@ type Database struct {
 
 func (database *Database) lazyInit() {
 	database.once.Do(func() {
-		host := os.Getenv("DEV_HOST")
-		port := os.Getenv("DEV_PORT")
-		dbname := os.Getenv("DEV_DBNAME")
-		username := os.Getenv("DEV_USERNAME")
-		password := os.Getenv("DEV_PASSWORD")
+		// host := os.Getenv("DEV_HOST")
+		// port := os.Getenv("DEV_PORT")
+		// dbname := os.Getenv("DEV_DBNAME")
+		// username := os.Getenv("DEV_USERNAME")
+		// password := os.Getenv("DEV_PASSWORD")
 
-		dsn := "host=" + host
-		dsn += " user=" + username
-		dsn += " password=" + password
-		dsn += " dbname=" + dbname
-		dsn += " port=" + port
+		// dsn := "host=" + host
+		// dsn += " user=" + username
+		// dsn += " password=" + password
+		// dsn += " dbname=" + dbname
+		// dsn += " port=" + port
 
-		// dsn := os.Getenv("DSN")
+		dsn := os.Getenv("DSN")
 
 		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
 		if err != nil {
